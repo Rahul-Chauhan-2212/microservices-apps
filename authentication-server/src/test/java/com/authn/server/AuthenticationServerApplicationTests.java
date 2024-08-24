fa3fa5e8fb8ddaac47de42c359c9c893f865c9e8
Rahul Chauhan <rchauhan9102@gmail.com>
2024-08-25 03:33:21 +0530

    Add package step in workflow

--- authentication-server/src/test/java/com/authn/server/AuthenticationServerApplicationTests.java
+++ authentication-server/src/test/java/com/authn/server/AuthenticationServerApplicationTests.java
@@ -1,14 +1,16 @@
 package com.authn.server;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthenticationServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-		//to test
-	}
+  @Test
+  void contextLoads() {
+    assertFalse(false);
+  }
 
 }
